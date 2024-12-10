33a59f71da65de73c49a65381ae89df9b7062931
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-12-10 18:12:40 +0530

    commit changes

---
+++ dist/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: { 
+        'heroBackgroundimg': "url('https://images.pexels.com/photos/4974916/pexels-photo-4974916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" 
+      }
+    },
+  },
+  plugins: [],
+}
+
